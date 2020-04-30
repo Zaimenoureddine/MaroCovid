739e241089bc4c8f681461ebcafd13af942f9f45
Noureddine Mac <noureddinemac@Noureddine-MacBook-Pro.local>
2020-04-30 23:52:01 +0000

    commit

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import HomeScreen from "./screen/HomeScreen";
+import RegionScreen from "./screen/RegionScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Home : HomeScreen,
+    Region : RegionScreen,
   },
-});
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "COVID MAROC ",
+    },
+  }
+);  
+
+
+
+export default createAppContainer(navigator);
+
+
+
+
